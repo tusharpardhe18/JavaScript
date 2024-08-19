cef835167dd617f1883ffc6a3ff16b572fecb930
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-08-20 01:23:26 +0530

    DataTypes and ECMA

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS Code as newer version
+
+//alert("hello") // we're using node, not browser 
+//alert(3+3)     // we're using node, not browser
+
+console.log(3
+    + 
+    3)  //code readiability should be high
+
+console.log("hello")
+
+let name = "Tushar"
+let age = 21
+let isloggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not declared
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Tushar"); //string
+console.log(typeof age);      //number
+console.log(typeof state);    //undefined
+console.log(typeof null);     //object
+// null is not an object, it's a primitive value
+
