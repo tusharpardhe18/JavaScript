b414fe325e58645e3ad3c5bd3a6b6cc57cf5dd26
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-08-21 04:54:08 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+/* 
+let score = "33abc"
+let score1 = null
+let score2 = undefined
+let score3 = true
+let score4 = "tushar"
+
+//const {score} = req.body;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+console.log(score4);
+
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+console.log(valueInNumber1)
+
+console.log(valueInNumber2)
+
+console.log(valueInNumber3)
+
+console.log(valueInNumber4)
+*/
+/*
+"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+true => 1, false => 0
+"" => 0, "tushar" => 1 
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
