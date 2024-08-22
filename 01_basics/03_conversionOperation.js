6ad08d886772afef067912377f6a4e36503ea782
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-08-22 06:21:05 +0530

    Why string to number conversion is confusing

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,9 +36,9 @@ console.log(valueInNumber4)
 true => 1, false => 0
 */
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
 /*
@@ -46,8 +46,67 @@ true => 1, false => 0
 "" => 0, "tushar" => 1 
 */
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// *******************OPERATIONS***********************
+// 1. Arithmetic Operations
+// 2. Comparison Operations
+// 3. Logical Operations
+// 4. Assignment Operations
+// 5. Bitwise Operations
+// 6. String Operations
+// 7. Conditional Operations
+// 8. Nullish Coalescing Operations
+// 9. Optional Chaining Operations
+// 10. Exponentiation Operations
+// 11. Multiplication Assignment Operations
+// 12. Division Assignment Operations
+// 13. Modulus Assignment Operations
+// 14. Addition Assignment Operations
+// 15. Subtraction Assignment Operations
+// 16. Left Shift Assignment Operations
+// 17. Right Shift Assignment Operations
+// 18. Bitwise AND Assignment Operations
+// 19. Bitwise OR Assignment Operations
+// 20. Bitwise XOR Assignment Operations
+// 21. Bitwise NOT Assignment Operations
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + 2 + 3 + 4);
+// console.log(1 + 2 + "3" + 4);
+
+
+console.log(+true); // 1
+console.log(+"");   // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// https://tc39.es/ecma262/#sec-type-conversion 
+
+
+
+
 
