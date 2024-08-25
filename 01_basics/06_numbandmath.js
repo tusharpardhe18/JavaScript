4b6474829b5dd06c0e1f9ee8d1cb3c264d2886d0
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-08-26 02:10:34 +0530

    Numbers and Math

---
+++ 01_basics/06_numbandmath.js
@@ -0,0 +1,49 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100.02);
+// console.log(balance);
+
+// console.log(balance.toString);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+const Num1 = 123.8966
+// console.log(Num1.toPrecision(4));
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++   MATHS ++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-44)); //positive value
+
+// console.log(Math.round(925.49326)); //round off
+
+// console.log(Math.ceil(4.2)); // uppar wali value
+
+// console.log(Math.floor(4.2)); //niche wali value
+
+// console.log(Math.sqrt(48)); // sqrt
+
+// console.log(Math.min(2,3,4,5)); //minimum
+
+// console.log(Math.max(3,4,5,6)); //maximum
+
+console.log(Math.random()); //random value
+
+console.log(Math.random() * 10);
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
