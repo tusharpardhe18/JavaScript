75157686852529956ce6bb09767ed8d4c04b7efb
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-09-13 07:09:41 +0530

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,7 +6,7 @@ if (true) {   // block scope
     const b = 20
     //var c = 30
     c = 30    
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     //output 10
        
 }
@@ -16,6 +16,57 @@ if (true) {   // block scope
     
 // }
 
-console.log(a);  // output 300
+// console.log(a);  // output 300
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+    // two()
+}
+
+one()
+
+if (true) {
+    const username = "hitesh"
+
+    if (username === "hitesh") {
+
+        const website = " youtube"
+
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// ********************** Interesting topic ******************
+
+
+
+// if we just declare the fucntion i like this then we can access the fucntion from anywhere like we can write the addOne(5) above it and execute it
+function addOne(num){
+    return num + 1;
+}
+
+addOne(5)
+
+
+// if we deckare the function inside a variable then we can't access the fucntion from anywhere, we have to call it after the declaration only or else it will give you error
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(4)
