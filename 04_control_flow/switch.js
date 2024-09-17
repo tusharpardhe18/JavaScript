95f3e09f75cbec367c0174fb651c514bc1b04279
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-09-17 06:54:44 +0530

    Switches in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "march"
+
+// switch (month) {
+//     case "january":
+//         console.log("january");
+//         break;
+//     case "feburary":
+//         console.log("feburary");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     case 8:
+//         console.log("august");
+//         break;
+//     case 9:
+//         console.log("september");
+//         break;
+//     case 10:
+//         console.log("october");
+//         break;
+//     case 11:
+//         console.log("november");
+//         break;
+//     case 12:
+//         console.log("december");
+//         break;
+//     default:
+//         console.log("not valid month");
+//         break;
+// }
+
