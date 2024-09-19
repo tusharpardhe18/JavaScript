98298448e2ecc7b10616f62de113692e417f2898
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-09-19 22:26:29 +0530

    While and Do-While Loops

---
+++ 05_Loops/two.js
@@ -0,0 +1,40 @@
+// while loops
+
+let i = 0
+
+while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+
+let myArray = ["superman", "flash","batman"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+
+// do while loop
+
+let score = 1
+
+do {
+    // console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
