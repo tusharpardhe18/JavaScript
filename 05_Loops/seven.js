06fa20d4e25a4a1ada2ce8154db6d1ae1d77af8e
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-09-22 17:19:14 +0530

    Maps in JS

---
+++ 05_Loops/seven.js
@@ -0,0 +1,26 @@
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = nums.map( (num) => num + 10 )
+
+// const newNums = nums.map( (num) => {return num + 10} )
+
+// const newNums = nums.map().map().filter()
+
+const newNums = nums
+                    .map( (num) => num*10 )
+                    .map( (num) => num + 1 )
+                    .filter( (num) => num >= 40 )
+
+
+
+
+console.log(newNums);
+
+
+
+
+
+
+
+
+
