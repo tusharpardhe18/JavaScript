4ce3412ed27d692138667f8e490ec9ed8d91ea06
Tushar Pardhe <tusharpardhe2018@gmail.com>
2024-09-26 00:28:15 +0530

    color changer

--- 07_Projects/01_ColorChanger/script.js
+++ 07_Projects/01_ColorChanger/script.js
@@ -6,5 +6,20 @@ buttons.forEach(function(button){
   button.addEventListener('click', function(event){
     console.log(event)
     console.log(event.target)
+    if(event.target.id === 'grey'){
+        body.style.backgroundColor = event.target.id;
+    }
+    if(event.target.id === 'white'){
+        body.style.backgroundColor = event.target.id;
+    }
+    if(event.target.id === 'blue'){
+        body.style.backgroundColor = event.target.id;
+    }
+    if(event.target.id === 'yellow'){
+        body.style.backgroundColor = event.target.id;
+    }
+    if(event.target.id === 'red'){
+        body.style.backgroundColor = event.target.id;
+    }
   });
 });
